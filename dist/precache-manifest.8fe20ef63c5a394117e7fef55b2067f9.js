303c84942e6ddf836ca6a97d2642c7cba0d0a67f
Martti Lepmets <mlepmets@gmail.com>
2019-06-24 22:56:55 +0300

    adding files

---
+++ dist/precache-manifest.8fe20ef63c5a394117e7fef55b2067f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2644bd2c0b3c4aeba35",
+    "url": "/css/app.10be5d86.css"
+  },
+  {
+    "revision": "a2644bd2c0b3c4aeba35",
+    "url": "/js/app.fd01109d.js"
+  },
+  {
+    "revision": "449810a74771c9bf053b",
+    "url": "/js/chunk-vendors.c8f74f4d.js"
+  },
+  {
+    "revision": "2e36289858b0e683a8c19abcd4d12652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
